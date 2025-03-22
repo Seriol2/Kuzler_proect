50ed141982c20e90bda3c8d417c26998ca3bf4e5
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-03-22 18:29:47 +0300

    Update views.py

--- pythonProject1/Kuzler_ver.1/views.py
+++ pythonProject1/Kuzler_ver.1/views.py
@@ -1,12 +1,1 @@
 from django.shortcuts import render
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from .models import Lesson
-
-
-class LessonView(APIView):
-    def get(self, request):
-        lessons = Lesson.objects.all()
-        return Response({"lessons": lessons})
