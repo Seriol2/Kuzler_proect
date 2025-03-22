0314f8f6925e10c0848d0cf0399e1208ec57d776
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-03-22 18:41:28 +0300

    Update admin.py

--- pythonProject1/Kuzler_ver.1/admin.py
+++ pythonProject1/Kuzler_ver.1/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from .models import Product, NFCTag
 
-from .models import Lesson
+# Регистрация моделей в административной панели
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')  # Показывать имя и цену в списке товаров
+    search_fields = ['name']          # Возможность поиска по имени товара
 
-admin.site.register(Lesson)
+@admin.register(NFCTag)
+class NFCTagAdmin(admin.ModelAdmin):
+    list_display = ('nfc_id', 'product', 'is_active')  # Показывать ID метки, связанный товар и статус активности
+    list_filter = ('is_active',)                       # Добавляем фильтр по активности метки
+    search_fields = ['nfc_id']                         # Возможность поиска по уникальному идентификатору метки
