96cd4356960f406b890aa486eeef692f45383844
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-03-22 18:34:33 +0300

    Update views.py

--- pythonProject1/Kuzler_ver.1/views.py
+++ pythonProject1/Kuzler_ver.1/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, NFCTagViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'nfc-tags', NFCTagViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
