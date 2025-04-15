df761f77e445a85f7904484614518310a17f25ba
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-04-15 20:33:04 +0300

    Delete kuzler directory

--- kuzler/products/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'price', 'audio_file']  # Убедись, что здесь есть ВСЕ нужные поля
--- kuzler/products/test_gtts.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-
-text = "Привет, как дела?"
-tts = gTTS(text=text, lang='ru')
-tts.save("output.mp3")
-
-print("✅ Аудио сохранено в output.mp3")
