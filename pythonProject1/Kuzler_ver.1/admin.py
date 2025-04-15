e6303df5e07b7d45f2ffa4a109b3e0a1cd13f3b6
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-04-15 18:27:55 +0300

    Delete pythonProject1 directory

--- pythonProject1/Kuzler_ver.1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-# Регистрируем модель Product в админке
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'price')  # Поля, которые отображаются в списке товаров
-    search_fields = ('code', 'name')  # Поиск по коду и названию товара
-    list_filter = ('price',)  # Фильтрация по цене товара
--- pythonProject1/Kuzler_ver.1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class StoreConfig(AppConfig):
-    name = 'store'  # Имя приложения
-    verbose_name = "Магазин"  # Человеко-понимаемое имя приложения (будет отображаться в админке Django)
