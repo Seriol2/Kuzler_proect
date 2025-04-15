6e38233cd05590dcea4f74c9a09ec09805d8b0ce
Seriol2 <145469680+Seriol2@users.noreply.github.com>
2025-04-15 18:26:53 +0300

    Delete kuzler directory

--- kuzler/products/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    audio_file = serializers.FileField(read_only=True)
-
-    class Meta:
-        model = Product
-        fields = ['nfc_id', 'name', 'price', 'audio_file']
